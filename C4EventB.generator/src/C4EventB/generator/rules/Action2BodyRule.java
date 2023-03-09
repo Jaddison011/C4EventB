a5f47622ef09fa7d207d98f20d3cab145c4a31d3
Jaddison011 <ja3g20@soton.ac.uk>
2023-03-09 14:10:52 +0000

    Changed project structure and added base spark generator files

---
+++ C4EventB.generator/src/C4EventB/generator/rules/Action2BodyRule.java
@@ -0,0 +1,80 @@
+package C4EventB.generator.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eventb.emf.core.machine.Action;
+import org.eventb.emf.core.machine.Event;
+import org.eventb.emf.core.machine.Machine;
+
+import ac.soton.emf.translator.TranslationDescriptor;
+import ac.soton.emf.translator.configuration.AbstractRule;
+import ac.soton.emf.translator.configuration.IRule;
+import ac.soton.eventb.spark.SparkExpression;
+import ac.soton.eventb.spark.SparkPackage;
+import ac.soton.eventb.spark.SparkPackageImp;
+import ac.soton.eventb.spark.SparkProcedure;
+import ac.soton.eventb.spark.SparkProcedureImp;
+
+
+import ac.soton.eventb.spark.generator.utils.SparkResourceUtils;
+import ac.soton.eventb.spark.generator.utils.SparkUtils;
+
+
+public class Action2BodyRule extends AbstractRule implements IRule {
+
+	protected static final EReference proc_body = SparkPackage.Literals.SPARK_SUB_PROGRAM_IMP__BODY;
+	protected static final EReference procedures = SparkPackage.Literals.SPARK_PACKAGE_IMP__PROCEDURES;
+	protected static final EReference packages = SparkPackage.Literals.SPARK_PROJECT__IMP_PACKAGES;
+
+	private String evt_name;
+	private SparkProcedureImp proc;
+	
+	@Override
+	public boolean enabled(EObject sourceElement) throws Exception {
+//		if(sourceElement instanceof Action) {
+//			 evt_name = ((Event) sourceElement.eContainer()).getName();
+//			if(evt_name.equals("INITIALISATION"))
+//				return false;
+//			else
+//				return true;				
+//		}				
+		return false;
+	}
+
+	@Override
+	public List<TranslationDescriptor> fire(EObject sourceElement, List<TranslationDescriptor> translatedElements)
+			throws Exception {
+		List<TranslationDescriptor> ret = new ArrayList<TranslationDescriptor>();
+
+		Action act = (Action) sourceElement;
+	//	TODO : check if we need to check the actions, if they need additional translation.
+	    SparkExpression body = SparkUtils.createExpression(act.getAction());   
+		ret.add(SparkUtils.descriptor(proc, proc_body , body, 3));
+
+		return ret;	
+	}
+
+	@Override
+	public boolean dependenciesOK(EObject sourceElement, List<TranslationDescriptor> translatedElements)
+			throws Exception {
+		
+	
+			Machine mch = (Machine) sourceElement.eContainer().eContainer();
+	
+			SparkPackageImp impPackage = (SparkPackageImp) SparkResourceUtils.findGeneratedElement(translatedElements, null, packages, mch.getName());
+		    proc = (SparkProcedureImp) SparkResourceUtils.findGeneratedElement(translatedElements, impPackage, procedures, evt_name);
+			if (impPackage != null && proc != null)
+				return true;
+			else
+				return false;
+	}
+	
+	@Override
+	public boolean fireLate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}   
---
+++ C4EventB.generator/src/C4EventB/generator/rules/Context2PackageRule.java
@@ -0,0 +1,93 @@
+package C4EventB.generator.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eventb.core.IMachineRoot;
+import org.eventb.emf.core.context.Context;
+import org.eventb.emf.core.machine.Machine;
+import org.eventb.emf.persistence.EventBEMFUtils;
+
+import ac.soton.emf.translator.TranslationDescriptor;
+import ac.soton.emf.translator.configuration.AbstractRule;
+import ac.soton.emf.translator.configuration.IRule;
+import ac.soton.eventb.spark.SparkPackage;
+import ac.soton.eventb.spark.SparkPackageSpec;
+import ac.soton.eventb.spark.SparkProject;
+import ac.soton.eventb.spark.generator.utils.SparkResourceUtils;
+import ac.soton.eventb.spark.generator.utils.SparkTranslatorUtils;
+import ac.soton.eventb.spark.generator.utils.SparkUtils;
+
+public class Context2PackageRule extends AbstractRule implements IRule {
+
+	protected static final EReference specPackages = SparkPackage.Literals.SPARK_PROJECT__SPEC_PACKAGES;
+	private SparkPackageSpec MchPckg;
+
+	@Override
+	public boolean enabled(EObject sourceElement) throws Exception {
+		if (sourceElement instanceof Machine) {
+			Machine mch = (Machine) sourceElement;
+			if (mch.getSees().isEmpty())
+				return false;
+			else
+				return true;
+		}
+
+		else
+			return false;
+
+	}
+
+	@Override
+	public List<TranslationDescriptor> fire(EObject sourceElement, List<TranslationDescriptor> translatedElements)
+			throws Exception {
+		List<TranslationDescriptor> ret = new ArrayList<TranslationDescriptor>();
+
+		SparkProject sparkProj = (SparkProject) SparkResourceUtils.findGeneratedElement(translatedElements, null, null,
+				"SPARK");
+		Machine mch = (Machine) sourceElement;
+
+		List<String> seenCxts = SparkTranslatorUtils.getSCSeenContextsStrings(mch);
+		for (String cxt_name : seenCxts) {
+			SparkPackageSpec specPackage = SparkUtils.createSpecPackage(sparkProj, cxt_name);
+			ret.add(SparkUtils.descriptor(null, specPackages, specPackage, 1));
+
+			// add with and use packages to machine package
+			MchPckg.getWithPackages().add(cxt_name);
+			MchPckg.getUsePackages().add(cxt_name);
+
+		}
+
+		return ret;
+	}
+
+	@Override
+	public boolean dependenciesOK(EObject sourceElement, List<TranslationDescriptor> translatedElements)
+			throws Exception {
+
+		SparkProject sparkProj = (SparkProject) SparkResourceUtils.findGeneratedElement(translatedElements, null, null,
+				"SPARK");
+
+		if (sparkProj == null)
+			return false;
+		else {
+			MchPckg = (SparkPackageSpec) SparkResourceUtils.findGeneratedElement(translatedElements, null, specPackages,
+					((Machine) sourceElement).getName());
+			if (MchPckg == null)
+				return false;
+			else
+				return true;
+		}
+
+	}
+
+	@Override
+	public boolean fireLate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
